4a15e9e9e5a91233c5c5f8c3bdb345fcdc4d2303
Jacob Edwards <jacobedwards1995@hotmail.co.uk>
2020-03-08 08:07:12 +0000

    standard project set up using spring initializer

---
+++ src/main/java/com/example/bytefitjava/bytefit/BytefitJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bytefitjava.bytefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytefitJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytefitJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bytefitjava/bytefit/BytefitJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bytefitjava.bytefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytefitJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
